730a61280997b5125e602838ef27c422dc2e08d7
4rthurmoura <160626279+4rthurmoura@users.noreply.github.com>
2024-10-08 12:32:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'Programação',
+'O que é JavaScript?',
+'o JavaScript é uma linguagem de programação'  
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+ )
+ criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa uma função'
+ )
+ criaCartao(
+    'Matemática',
+    'Qual o resutado de 2 x 2?',
+    'o resultado é 4' 
+)
